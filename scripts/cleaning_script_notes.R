9528166f563b151b18ae6607e7ff04e5a78c79b6
Louise <louisegreen18@gmail.com>
2022-02-07 10:50:16 +0000

    cleaning script started
    cleaning script started

---
+++ scripts/cleaning_script_notes.R
@@ -0,0 +1,16 @@
+# Notes for cleaning script
+
+# Cleaning to complete on domestic_tourism
+# - janitor on names
+# - drop 'measurement' column
+# - drop 'featurecode' column
+# - split into 3 sets? pounds, visits and nights?
+#   - mutate DateCode to specify individual years
+
+# Cleaning of activities
+# - janitor on names
+# - drop 'featurecode'
+# - drop measurement
+# - figure out how to group / sort this (the question is which type generates the most visits?)
+
+

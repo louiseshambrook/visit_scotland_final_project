78138bfa58f477410c85e5b2f277a0ca9fb9ff00
Louise <louisegreen18@gmail.com>
2022-02-08 15:54:21 +0000

    cleaning up cleaning script
    cleaning up the code in the cleaning script

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -49,7 +49,16 @@ location <- read_csv("raw_data/tourism_day_visits_location.csv") %>%
   clean_names() %>%
   select(-feature_code, -measurement) %>%
   rename(year = "date_code",
-         unit = "units")
+         unit = "units") %>%
+  mutate(type_of_location_visited = 
+           case_when(type_of_location_visited == "City/large town" ~ "City/large town",
+                     type_of_location_visited == "Rural countryside" ~ "Rural countryside",
+                     type_of_location_visited == "Seaside - beach" ~ "Seaside",
+                     type_of_location_visited == "Seaside - other" ~ "Seaside",
+                     type_of_location_visited == "Seaside resort or town" ~ "Seaside",
+                     type_of_location_visited == "Small town" ~ "Small town",
+                     type_of_location_visited == "Village" ~ "Village",
+                     type_of_location_visited == "All areas" ~ "Other"))
 write_csv(location, "clean_data/location_clean.csv")
 rm(location)
 
@@ -75,9 +84,9 @@ transport <- read_csv("raw_data/tourism_day_visits_transport.csv") %>%
                      method_of_travel == "Other" ~ "Other",
                      method_of_travel == "Plane" ~ "Plane",
                      method_of_travel == "Taxi" ~ "Taxi",
-                     method_of_travel == "Train" ~ "Train/Tube/Tram",
-                     method_of_travel == "Tram" ~ "Train/Tube/Tram",
-                     method_of_travel == "Tube" ~ "Train/Tube/Tram",
+                     method_of_travel == "Train" ~ "Train",
+                     method_of_travel == "Tram" ~ "Tram",
+                     method_of_travel == "Tube" ~ "Tube",
                      method_of_travel == "Walked" ~ "Walked",
                      method_of_travel == "All" ~ "Other"))
 write_csv(transport, "clean_data/transport_clean.csv")
@@ -92,11 +101,11 @@ accomodation <- read_csv("raw_data/scottish_accomodation_occupancy.csv") %>%
   rename(year = "date_code",
          unit = "units") %>%
   mutate(location = 
-           case_when(location == "Accessible Rural" ~ "Rural countryside",
+           case_when(location == "Accessible Rural" ~ "Rural",
                      location == "Accessible Small Towns" ~ "Small town",
-                     location == "Large Urban Areas" ~ "City/large town",
-                     location == "Other Urban Areas" ~ "City/large town",
-                     location == "Remote Rural" ~ "Remote rural countryside",
+                     location == "Large Urban Areas" ~ "Urban area",
+                     location == "Other Urban Areas" ~ "Urban area",
+                     location == "Remote Rural" ~ "Remote rural",
                      location == "Remote Small Towns" ~ "Remote small town",
                      location == "All" ~ "Other"))
 write_csv(accomodation, "clean_data/accomodation_clean.csv")

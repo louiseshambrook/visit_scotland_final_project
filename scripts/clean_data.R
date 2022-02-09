bf1adeb50bb62a1b05281a97302083d606186463
Louise <louisegreen18@gmail.com>
2022-02-09 16:57:35 +0000

    cleaning script tidied
    cleaning script code tidied

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -10,24 +10,51 @@ activities <- read_csv("raw_data/tourism_day_visits_activities.csv") %>%
   select(-feature_code, -measurement) %>%
   rename(year = "date_code",
          unit = "units") %>%
-  mutate(activity_type = 
-           case_when(tourism_activity == "Shopping for items that you do not regularly buy" ~ "Shopping",
-                     tourism_activity == "Watched live sporting events (not on TV)" ~ "Watch live sports",
-                     tourism_activity == "Night out to a bar, pub and/or club" ~ "Night out",
-                     tourism_activity == "Special personal events e.g. wedding, graduation" ~ "Attend personal event",
-                     tourism_activity == "Leisure activities e.g. hobbies & evening classes" ~ "Leisure activity",
-                     tourism_activity == "Sport participation, e.g. exercise classes, gym" ~ "Sports participation",
-                     tourism_activity == "Special public event e.g. festival, exhibition" ~ "Attend public event",
+  mutate(activity_type = case_when(
+                    tourism_activity ==
+                       "Shopping for items that you do not regularly buy"
+                     ~ "Shopping",
+                     tourism_activity ==
+                       "Watched live sporting events (not on TV)"
+                     ~ "Watch live sports",
+                     tourism_activity == "Night out to a bar, pub and/or club"
+                     ~ "Night out",
+                     tourism_activity ==
+                       "Special personal events e.g. wedding, graduation"
+                     ~ "Attend personal event",
+                     tourism_activity ==
+                       "Leisure activities e.g. hobbies & evening classes"
+                     ~ "Leisure activity",
+                     tourism_activity ==
+                       "Sport participation, e.g. exercise classes, gym"
+                     ~ "Sports participation",
+                     tourism_activity ==
+                       "Special public event e.g. festival, exhibition"
+                     ~ "Attend public event",
                      tourism_activity == "Went out for a meal" ~ "Eating out",
-                     tourism_activity == "Day out to a beauty/health centre/spa, etc." ~ "Day out (spa)",
-                     tourism_activity == "Outdoor leisure activities e.g. walking, golf" ~ "Outdoor leisure activity",
-                     tourism_activity == "Visitor attraction e.g. theme park, museum, zoo" ~ "Visitor attraction",
-                     tourism_activity == "General day out/ to explore an area" ~ "Day out",
-                     tourism_activity == "Entertainment - to a cinema, concert or theatre" ~ "Entertainment - cinema",
-                     tourism_activity == "Day trips/excursions for other leisure purpose" ~ "Day trip - leisure",
-                     tourism_activity == "Visited friends or family for leisure" ~ "Visit family/friends",
+                     tourism_activity ==
+                       "Day out to a beauty/health centre/spa, etc."
+                     ~ "Day out (spa)",
+                     tourism_activity ==
+                       "Outdoor leisure activities e.g. walking, golf"
+                     ~ "Outdoor leisure activity",
+                     tourism_activity ==
+                       "Visitor attraction e.g. theme park, museum, zoo"
+                     ~ "Visitor attraction",
+                     tourism_activity ==
+                       "General day out/ to explore an area" ~ "Day out",
+                     tourism_activity ==
+                       "Entertainment - to a cinema, concert or theatre"
+                     ~ "Entertainment - cinema etc.",
+                     tourism_activity ==
+                       "Day trips/excursions for other leisure purpose"
+                     ~ "Day trip - leisure",
+                     tourism_activity ==
+                       "Visited friends or family for leisure"
+                     ~ "Visit family/friends",
                      tourism_activity == "All" ~ "Other")) %>%
-  mutate(unit = ifelse(unit == "million pounds (GBP)", "million (£)", "million")) %>%
+  mutate(unit = ifelse(unit == "million pounds (GBP)",
+                       "million (£)", "million")) %>%
   relocate(value, .after = year) %>%
   relocate(breakdown_of_domestic_tourism, .after = unit) %>%
   select(-tourism_activity)
@@ -41,7 +68,8 @@ demographics <- read_csv("raw_data/tourism_day_visits_demographics.csv") %>%
   select(-feature_code, -measurement) %>%
   rename(year = "date_code",
          unit = "units") %>%
-  mutate(unit = ifelse(unit == "million pounds (GBP)", "million (£)", "million")) %>%
+  mutate(unit = ifelse(unit == "million pounds (GBP)",
+                       "million (£)", "million")) %>%
   relocate(value, .after = year) %>%
   relocate(breakdown_of_domestic_tourism, .after = unit)
 write_csv(demographics, "clean_data/demographics_clean.csv")
@@ -55,15 +83,19 @@ location <- read_csv("raw_data/tourism_day_visits_location.csv") %>%
   rename(year = "date_code",
          unit = "units") %>%
   mutate(type_of_location_visited = 
-           case_when(type_of_location_visited == "City/large town" ~ "City/large town",
-                     type_of_location_visited == "Rural countryside" ~ "Rural countryside",
+           case_when(type_of_location_visited == "City/large town"
+                     ~ "City/large town",
+                     type_of_location_visited == "Rural countryside"
+                     ~ "Rural countryside",
                      type_of_location_visited == "Seaside - beach" ~ "Seaside",
                      type_of_location_visited == "Seaside - other" ~ "Seaside",
-                     type_of_location_visited == "Seaside resort or town" ~ "Seaside",
+                     type_of_location_visited == "Seaside resort or town"
+                     ~ "Seaside",
                      type_of_location_visited == "Small town" ~ "Small town",
                      type_of_location_visited == "Village" ~ "Village",
                      type_of_location_visited == "All areas" ~ "Other")) %>%
-  mutate(unit = ifelse(unit == "million pounds (GBP)", "million (£)", "million")) %>%
+  mutate(unit = ifelse(unit == "million pounds (GBP)",
+                       "million (£)", "million")) %>%
   relocate(value, .after = year) %>%
   relocate(breakdown_of_domestic_tourism, .after = unit)
 write_csv(location, "clean_data/location_clean.csv")
@@ -94,7 +126,12 @@ transport <- read_csv("raw_data/tourism_day_visits_transport.csv") %>%
                      method_of_travel == "Tram" ~ "Tram",
                      method_of_travel == "Tube" ~ "Tube",
                      method_of_travel == "Walked" ~ "Walked",
-                     method_of_travel == "All" ~ "Other"))
+                     method_of_travel == "All" ~ "Other")) %>%
+  mutate(unit = ifelse(unit == "million pounds (GBP)",
+                       "million (£)", "million")) %>%
+  relocate(value, .after = year) %>%
+  relocate(breakdown_of_domestic_tourism, .after = unit) %>%
+  select(-method_of_travel)
 write_csv(transport, "clean_data/transport_clean.csv")
 rm(transport)
 
@@ -113,7 +150,10 @@ accomodation <- read_csv("raw_data/scottish_accomodation_occupancy.csv") %>%
                      location == "Remote Rural" ~ "Remote rural",
                      location == "Remote Small Towns" ~ "Remote small town",
                      location == "All" ~ "Other")) %>%
-  separate(accommodation_type_and_occupancy, into = c("accommodation_type", "occupancy_type"), sep = "-")
+  separate(accommodation_type_and_occupancy,
+           into = c("accommodation_type","occupancy_type"), sep = "-") %>%
+  mutate(unit = ifelse(unit == "Percentage", "percentage", NA)) %>%
+  relocate(value, .after = year)
 write_csv(accomodation, "clean_data/accomodation_clean.csv")
 rm(accomodation)
 
